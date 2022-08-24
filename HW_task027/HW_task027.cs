7b0d63d5e93f9c72c9dc10b29d324ea44f42f604
krenartem <krenartem@yandex.ru>
2022-08-24 23:42:20 +0300

    add homework task 027 with cicles <if> and <while>

---
+++ HW_task027/HW_task027.cs
@@ -0,0 +1,24 @@
+﻿// принимает на вход число и выдает сумм цифр в числе
+Console.Clear();
+Console.Write("Введите любое целое число, программа посчитает сумму цифр в числе: ");
+int number = int.Parse(Console.ReadLine());
+int initNumber = number;
+int sum = 0;
+if (initNumber>0)
+{
+   while (number != 0)
+    {
+        sum = sum + number % 10;
+        number /= 10;
+    }
+}
+else
+{
+    while (number != 0)
+    {
+        sum = sum + number % 10;
+        number /= 10;
+    }
+    sum *= -1;
+}
+Console.WriteLine($"Сумма цифр в числе {initNumber} = {sum}");
